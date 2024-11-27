e759c3f132b66160929d7dd2e65dcab214ff2cca
op200 <112297583+op200@users.noreply.github.com>
2024-11-27 14:42:40 +0800

    add VideoTimeMatch.lua v0.1

---
+++ VideoTimeMatch.lua
@@ -0,0 +1,89 @@
+﻿local gt=aegisub.gettext
+
+script_name = gt"Video Time Match"
+script_description = gt"According to frame difference from two videos to match time. (Aegisub must load this video, the FPS of both videos must be the same and must be CFR)"
+script_author = "op200"
+script_version = "0.1"
+-- https://github.com/op200/Tag-Replace_for_Aegisub
+
+local function debug(text, to_exit)
+	local button = aegisub.dialog.display({{class='label', label=tostring(text):gsub("&", "&&")}})
+	if not button or to_exit then aegisub.cancel() end
+end
+
+local function find_event(sub)
+	for i=1,#sub do
+		if sub[i].section=="[Events]" then
+			return i
+		end
+	end
+end
+
+local time_table = {}
+
+local function get_time_table()
+	local button, input = aegisub.dialog.display({
+		{name='entry', height=4, width=5, class='textbox', hint=gt"Enter manually here"},
+		{name='select', y=4, width=5, class='dropdown', items={gt"Manual entry", gt"Auto run VideoMatch"}, value="Auto run VideoMatch", hint=gt"If manual entry, use the textbox's content, else then run VideoMatch.exe"},
+		{name='video1', y=5, width=5, class='edit', hint=gt"Enter video path 1 here"},
+		{name='video2', y=6, width=5, class='edit', hint=gt"Enter video path 2 here"},
+		{name='option', y=7, width=5, class='edit', text="-scale 4", hint=gt"Enter additional options here"}
+	})
+	if not button then aegisub.cancel() end
+
+	local frame_result
+	if input.select=="Manual entry" then
+		frame_result = input.entry
+	else
+		local handle = io.popen('videomatch -i1 "'..input.video1:gsub('"','')..'" -i2 "'..input.video2:gsub('"','')..'" '..input.option.." & pause")
+		frame_result = handle:read("*a")
+		handle:close()
+	end
+
+	for line in frame_result:gmatch("[^\n]+") do
+		local a,b = line:match("(%d+)%->([%d-]+)")
+		if not a then break end
+		time_table[tonumber(a)] = tonumber(b)
+	end
+end
+
+local function match_time(sub)
+	local begin = find_event(sub)
+
+	for i = begin,#sub do
+		local line = sub[i]
+
+		local frame_1, frame_2 = aegisub.frame_from_ms(line.start_time), aegisub.frame_from_ms(line.end_time)
+		frame_1, frame_2 = time_table[frame_1], time_table[frame_2]
+		local start_time, end_time = aegisub.ms_from_frame(frame_1), aegisub.ms_from_frame(frame_2)
+		line.start_time, line.end_time = start_time or line.start_time, end_time or line.end_time
+
+		line.actor = line.actor.."[VTM]"
+
+		sub[i] = line
+	end
+
+	aegisub.set_undo_point("Match time and mark [VTM]")
+
+	for i = begin,#sub do
+		local line = sub[i]
+
+		line.actor = line.actor:sub(1, -6)
+
+		sub[i] = line
+	end
+
+	aegisub.set_undo_point("Clear [VTM]")
+end
+
+local function macro_processing_function(sub)
+	if not os.execute("videomatch -v") then
+		debug("This script need VideoMatch.exe", true)
+	end
+
+	get_time_table()
+
+	match_time(sub)
+end
+
+aegisub.register_macro(script_name, script_description, macro_processing_function)
